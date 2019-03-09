f3f89fa966d81e416dc23af32afc6b608c248278
Glitch (rb-wk10-lab-complete) <none>
2019-03-09 21:30:17 +0000

    🚡😿 Updated with Glitch

---
+++ routes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const start = require('./controllers/start');
+const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
+const accounts = require('./controllers/accounts.js');
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+router.post('/dashboard/uploadpicture', dashboard.uploadPicture);
+
+router.get('/dashboard', dashboard.index);
+router.get('/about', about.index);
+router.get('/dashboard/deleteallpictures', dashboard.deleteAllPictures);
+router.get('/dashboard/deletepicture', dashboard.deletePicture);
+
+module.exports = router;
